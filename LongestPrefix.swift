f2a57daf170172d090b526f9c4ab3b275f3c1852
Ashin Asok <ashin9895@gmail.com>
2019-06-21 15:14:48 +0530

    edits

--- LongestPrefix.swift
+++ LongestPrefix.swift
@@ -1,26 +1,43 @@
-//Longest Common Prefix
+/* Write a function to find the longest common prefix string amongst an array of strings.
 
-class CommonPrefix : NSObject {
+If there is no common prefix, return an empty string "".
 
-func longestCommonPrefix(_ strs: [String]) -> String {
-    
-    var commonString : String = strs[0]
-    var index : Int = 1
-    
-    while index <= (strs.count - 1){
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+Note:
+
+All given inputs are in lowercase letters a-z.
+*/
+
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
         
-        if commonString == ""{
-            index = strs.count
-        }else if strs[index].hasPrefix(commonString){
-            index += 1
-        }else{
-            commonString.removeLast()
+        var index : Int = 1
+        
+        if strs.count == 0{
+            return ""
         }
         
+        var commonString : String = strs[0]
+        while index <= (strs.count - 1){
+        
+            if commonString == ""{
+                index = strs.count
+            }else if strs[index].hasPrefix(commonString){
+                index += 1
+            }else{
+                commonString.removeLast()
+            }
+        
+        }
+    
+        return commonString
     }
-    
-    return commonString
-    
-}
-
 }
